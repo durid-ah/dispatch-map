ebc3e933981d9a25b240485abfe60d75adfbf27a
Durid Ahmad <durid.ahmad.dev@gmail.com>
2026-07-15 21:05:42 -0400

    consumer: db class and first query

---
+++ packages/dispatch-consumer/db.py
@@ -0,0 +1,24 @@
+from sqlmodel import create_engine, Session, Engine, select
+from models import Event
+
+class DB:
+    url: str
+    engine: Engine
+    session: Session
+
+    def __init__(self, url: str):
+        self.url = url
+
+    def __enter__(self):
+        self.engine = create_engine(self.url)
+        self.session = Session(self.engine)
+        return self
+
+    def get_events(self, external_ids: list[str]) -> list[Event]:
+        return self.session \
+            .exececute(select(Event).where(Event.external_id.in_(external_ids))) \
+            .all()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.session.close()
+        self.engine.dispose()
