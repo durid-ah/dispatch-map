7c3d826bccab80f81063dfc96f542cefc17e1b5f
Durid Ahmad <durid.ahmad.dev@gmail.com>
2026-07-21 17:31:40 -0400

    api: test boilerplate

--- packages/api/main.py
+++ packages/api/main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from api!")
+from collections.abc import AsyncIterable, Iterable
+
+from fastapi import FastAPI
+from fastapi.sse import EventSourceResponse
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 
 
-if __name__ == "__main__":
-    main()
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class Item(BaseModel):
+    name: str
+    description: str | None
+
+
+items = [
+    Item(name="Plumbus", description="A multi-purpose household device."),
+    Item(name="Portal Gun", description="A portal opening device."),
+    Item(name="Meeseeks Box", description="A box that summons a Meeseeks."),
+]
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/stream", response_class=EventSourceResponse)
+async def sse_items() -> AsyncIterable[Item]:
+    for item in items:
+        yield item
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
